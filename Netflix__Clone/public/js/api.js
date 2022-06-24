120a24446f90b10f58f46417aff6226a7ede747d
SIDDHANT GAUR <siddhantgaur881@gmail.com>
2022-06-24 08:31:06 +0530

    node_modules

---
+++ Netflix__Clone/public/js/api.js
@@ -0,0 +1,9 @@
+
+let api_key = "66a8a6c656220c1c5306061e5eeb56ee";
+
+let img_url = "https://image.tmdb.org/t/p/w500";
+let original_img_url = "https://image.tmdb.org/t/p/original";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
+let movie_detail_http = "https://api.themoviedb.org/3/movie";
+
